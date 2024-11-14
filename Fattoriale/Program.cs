4d6f3b68fa1c1c9e8535339e0e9f1f145f8ecf5b
Davide Luppoli <davide@davideluppoli.it>
2024-11-14 10:58:52 +0100

    Fattoriale parallelo

--- Fattoriale/Program.cs
+++ Fattoriale/Program.cs
@@ -16,9 +16,27 @@ namespace Fattoriale
 
             Console.WriteLine(FattorialeSincrono(n));
 
-            ulong r1 = FattorialeParziale(1, 5);
-            ulong r2 = FattorialeParziale(6, n);
-            Console.WriteLine(r1*r2);
+            uint range = n / tasks;
+            List<Task<ulong>> tasklist = new List<Task<ulong>>();
+
+            for (uint i = 0; i< tasks; i++)
+            {
+                uint local_i = i;
+                tasklist.Add(
+                    Task.Run( () => FattorialeParziale( range* local_i + 1, (local_i + 1)*range ) )
+                );
+            }
+            if( n % tasks!=0)
+                tasklist.Add(
+                    Task.Run(() => FattorialeParziale(tasks*range + 1, n))
+                );
+            
+            Task.WaitAll(tasklist.ToArray());
+
+            ulong risultato = 1;
+            tasklist.ForEach(t => risultato*=t.Result);
+
+            Console.WriteLine(risultato);
 
             Console.ReadLine();
         }
