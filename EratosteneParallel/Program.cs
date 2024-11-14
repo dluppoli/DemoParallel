b509ed74bc5e0f7e985d33652f7c1e6797bbca11
Davide Luppoli <davide@davideluppoli.it>
2024-11-14 09:42:40 +0100

    Eratostene

---
+++ EratosteneParallel/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EratosteneParallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100_000_000;
+            EratosteneSequenziale(n);
+            //EratosteneParalleloThread(n);
+            EratosteneParalleloTask(n);
+            Console.ReadLine();
+        }
+
+        static void EratosteneSequenziale(int n)
+        {
+            bool[] numeri = new bool[n+1];
+            for(int i = 0; i < numeri.Length;i++)
+                numeri[i] = true;
+            
+            numeri[0] = false;
+            numeri[1] = false;
+
+            var sw = Stopwatch.StartNew();
+            for(int i = 2; i<= Math.Sqrt(n); i++)
+            {
+                for (int j = i * 2; j <= n; j += i)
+                    numeri[j] = false;
+            }
+            sw.Stop();
+            Console.WriteLine($"Trovati {numeri.Count(num => num==true)} numeri primi in {sw.ElapsedMilliseconds}ms");
+        }
+
+        static void EratosteneParalleloThread(int n)
+        {
+            bool[] numeri = new bool[n + 1];
+            for (int i = 0; i < numeri.Length; i++)
+                numeri[i] = true;
+
+            numeri[0] = false;
+            numeri[1] = false;
+
+
+            List<Thread> threads = new List<Thread>();
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                int lolcal_i = i;
+                var t = new Thread(p =>
+                {
+                    for (int j = lolcal_i * 2; j <= n; j += lolcal_i)
+                        numeri[j] = false;
+                });
+                t.Start();
+                threads.Add(t);
+            }
+            threads.ForEach(t => t.Join());
+
+            sw.Stop();
+            Console.WriteLine($"Trovati {numeri.Count(num => num == true)} numeri primi in {sw.ElapsedMilliseconds}ms");
+        }
+
+        static void EratosteneParalleloTask(int n)
+        {
+            bool[] numeri = new bool[n + 1];
+            for (int i = 0; i < numeri.Length; i++)
+                numeri[i] = true;
+
+            numeri[0] = false;
+            numeri[1] = false;
+
+
+            List<Task> tasks = new List<Task>();
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                int lolcal_i = i;
+                var t = Task.Run(() =>
+                {
+                    for (int j = lolcal_i * 2; j <= n; j += lolcal_i)
+                        numeri[j] = false;
+                });
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+
+            sw.Stop();
+            Console.WriteLine($"Trovati {numeri.Count(num => num == true)} numeri primi in {sw.ElapsedMilliseconds}ms");
+        }
+    }
+}
